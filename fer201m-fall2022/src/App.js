78675aefad81c27225f206a8cf9ca379f5ce0a59
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2022-09-17 13:48:56 +0700

    Presentational and Container Components

--- fer201m-fall2022/src/App.js
+++ fer201m-fall2022/src/App.js
@@ -1,44 +1,59 @@
-// import logo from './logo.svg';
-// import './App.css';
+// // import logo from './logo.svg';
+// // import './App.css';
 
-// function App() {
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// import './App.css';
+// import Navigation from './components/Navigation';
+// import Players from './components/Players';
+// import Footer from './components/Footer';
+// function App(){
 //   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
+//     <div className='App'>
+//     <Navigation />
+//     <Players />
+//     <Footer />
 //     </div>
 //   );
 // }
 
+
+
 // export default App;
 
 import './App.css';
-import Navigation from './components/Navigation';
-import Players from './components/Players';
 import Footer from './components/Footer';
-function App(){
+import Main from './components/Main';
+import Navigation from './components/Navigation';
+function App() {
   return (
     <div className='App'>
-    <Navigation />
-    <Players />
-    <Footer />
+      <Navigation/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
-
-
-
 export default App;
 
